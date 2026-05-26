8e4be47c430365d18407288476d22f6fef9699b9
Arjun Asthana <arjunasthana02@gmail.com>
2026-05-27 02:37:34 +0530

    test app

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,19 @@
-import { useState } from "react";
-
-import Intro from "./pages/Intro";
-import Home from "./pages/Home";
-import Moods from "./pages/Moods";
-import Todo from "./pages/Todo";
-
-function App(){
-
-const [screen,setScreen]=useState("intro")
-
-if(screen==="intro"){
-
-return(
-<Intro
-enterWorld={()=>{
-setScreen("home")
-}}
-/>
-)
-
-}
-
-if(screen==="home"){
-
-return(
-<Home
-openMoods={()=>{
-setScreen("moods")
-}}
-openTodo={()=>{
-setScreen("todo")
-}}
-/>
-)
-
-}
-
-if(screen==="todo"){
-
-return(
-<Todo
-goBack={()=>{
-setScreen("home")
-}}
-/>
-)
-
-}
-
-return(
-
-<Moods
-goBack={()=>{
-setScreen("home")
-}}
-/>
-
-)
-
+function App() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#111",
+        color: "white",
+        fontSize: "40px",
+      }}
+    >
+      Birthday Website 🎂🔥
+    </div>
+  )
 }
 
 export default App
